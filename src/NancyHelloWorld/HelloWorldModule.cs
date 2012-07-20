04c3907ae7ff77fedd81fc797cb415fce0698b27
rabitarochan <rabitarochan@gmail.com>
2012-07-20 13:08:29 +0900

    first commit.

---
+++ src/NancyHelloWorld/HelloWorldModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+
+namespace NancyHelloWorld
+{
+    public class HelloWorldModule : NancyModule
+    {
+        public HelloWorldModule()
+        {
+            // 以下のアドレスでアクセスされた場合
+            // http://localhost:9001/helloworld
+            // http://localhost:9001/helloworld/
+            Get["/"] = parameter => {
+                return "Hello world!!";
+            };
+
+            // 以下のアドレスでアクセスされた場合
+            // http://localhost:9001/helloworld/{name}
+            Get["/{name}"] = parameter => {
+                return string.Format("Hi {0}, Hello world!!", parameter.name);
+            };
+        }
+    }
+}
---
+++ src/NancyHelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace NancyHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // サーバのインスタンスを作成
+            var host = new NancyHost(
+                new Uri("http://localhost:9001/helloworld/")
+            );
+
+            // サーバ開始
+            host.Start();
+            
+            // 入力を待つ
+            Console.ReadLine();
+
+            // サーバ定義
+            host.Stop();
+        }
+    }
+}
